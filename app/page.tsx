0fe12d75fb106a4983061b9e80c5b4b48ba98b73
EgehanKorkmaz <egehankrkmz@gmail.com>
2024-11-05 23:48:35 +0300

    Add theme provider and grid component; remove unused SVGs and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import { FloatingNav } from '@/components/ui/floating-navbar';
 import Hero from '../components/Hero';
+import { FaHome } from 'react-icons/fa';
+import Grid from '@/components/Grid';
 
 export default function Home() {
   return (
-    <main className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black flex justify-center items-center 
+    flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className="max-w-7xl w-full">
+            <FloatingNav navItems={
+                [
+                    { name: 'Home', link: '/', icon: <FaHome/> },
+                    // { name: 'About', link: '#about' },
+                    // { name: 'Projects', link: '#projects' },
+                    // { name: 'Contact', link: '#contact' },
+                ]
+            }/>
             <Hero />
+            <Grid />
         </div>
     </main>
   );
