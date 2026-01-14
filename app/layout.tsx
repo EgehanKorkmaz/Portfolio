fd33d37658c6006edcadc85c380665960b66afcb
Egehan <egehankrkmz@gmail.com>
2026-01-14 05:44:19 +0300

    Google Analytics scripts added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
@@ -25,6 +26,19 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/exp1.svg" sizes="any" />
       </head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-PPY1J0MG1W"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-PPY1J0MG1W');
+        `}
+      </Script>
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
